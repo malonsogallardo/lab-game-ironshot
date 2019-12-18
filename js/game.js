a510f47660808e9a07b8c16fcff145941aa57c98
Miguel <alonsogalalrdomiguel@gmail.com>
2019-12-18 15:50:34 +0100

    edit background

--- js/game.js
+++ js/game.js
@@ -7,9 +7,9 @@ class Game {
     this.fps = 60;
     this.framesCounter = 0;
 
-    this.live = 15;
+    this.live = 5;
     this.punt = 0;
-    this.countElement = 25;
+    this.countElement = 5;
     this.died = false;
     
     this.background = new Background(this.ctx, this.width, this.heigth);
@@ -33,14 +33,17 @@ class Game {
       this.draw();
       this.move();
 
-      this.clearElements();      
-      this.isCollision()
-      this.checkLive()
-      this.countLives();
-
-      if(this.framesCounter % 60 === 0) this.generateElements()
       
+      this.isCollision();
+      this.clearElements();  
+      this.countLives();  
+      //this.checkLive(); 
+      console.log(this.framesCounter)
+      if(this.framesCounter % 60 === 0) this.generateElements();
       if(this.framesCounter > 1000) this.framesCounter = 0;
+      console.log(this.elements.length)
+   
+
 
     }, 1000 / this.fps);
 
@@ -65,16 +68,12 @@ class Game {
   }
 
   generateElements() {
-    if(this.countElement > -1){
+    if(this.countElement > 0){
       let images = ["images/css_logo.png","images/html_logo.png",  "images/blackhtml_logo.png",  "images/js_logo.png"];
       this.elements.push(new Element(this.ctx, this.width, this.height, images[Math.floor(Math.random()*images.length)]))
       this.countElement--
 
-    } else {
-
-     if(this.elements[this.elements.length-1].posX >= 1620) {}
-
-  }
+    } 
 }
 
   clearElements() {
@@ -98,6 +97,7 @@ class Game {
       for(let j = 0; j < this.tower.bullets.length; j++) {
         let bullet = this.tower.bullets[j]
         if(element.posX + element.width > bullet.posX && bullet.posX + bullet.width > element.posX && element.posY + element.heigth > bullet.posY && bullet.posY + bullet.heigth > element.posY) {
+          (console.log(element.posX + element.width > bullet.posX))
           this.elements.splice(this.elements.indexOf(element), 1)
           this.tower.bullets.splice(this.tower.bullets.indexOf(bullet), 1)
           this.punt++
@@ -107,23 +107,17 @@ class Game {
     }
   }
 
-  checkLive(){
-    if(this.countElement === -1 && this.died === false){this.win()}
-  }
-  
-
   countLives() {
     if (this.elements.some( element => {
      return element.posX >= 1620
-
     })) {
       this.live--;
       console.log(this.live)
     }
 
-
-    if(this.live === -1){
+    if(this.live === 0){
       this.gameOver()
     }
+    if(this.elements.length === 0 && this.countElement === 0){this.win()}
   }
 }
--- js/score.js
+++ js/score.js
@@ -15,14 +15,14 @@ class Score{
     draw(punt){
         this.ctx.drawImage(this.image1, 500,5,50,50);
         this.ctx.fillStyle = "#265897"
-        this.ctx.font = "40px comic-sans"
+        this.ctx.font = "40px Georgia"
         this.ctx.fillText(punt, 570, 42)
     }
 
     drawLive(live){
         this.ctx.drawImage(this.image, 300,0,50,50);
         this.ctx.fillStyle = "#EF011E"
-        this.ctx.font = "40px comic-sans"
+        this.ctx.font = "40px Georgia"
         this.ctx.fillText(live, 370, 39)
         
     }
